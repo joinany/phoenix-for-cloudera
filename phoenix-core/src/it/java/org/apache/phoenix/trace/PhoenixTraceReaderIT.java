04ff222d78c004d96b80c9ca7f1a75b3c8ae57b4
Chun Zeng <zeng.chun@hotmail.com>
2017-03-28 15:13:55 +0800

    Fixups in phoenix-core and phoenix-assembly for HTrace

--- phoenix-core/src/it/java/org/apache/phoenix/trace/PhoenixTraceReaderIT.java
+++ phoenix-core/src/it/java/org/apache/phoenix/trace/PhoenixTraceReaderIT.java
@@ -39,6 +39,7 @@ import org.apache.htrace.Span;
 import org.apache.phoenix.metrics.MetricInfo;
 import org.apache.phoenix.trace.TraceReader.SpanInfo;
 import org.apache.phoenix.trace.TraceReader.TraceHolder;
+import org.apache.phoenix.trace.TracingUtils;
 import org.junit.Test;
 
 /**
@@ -61,7 +62,7 @@ public class PhoenixTraceReaderIT extends BaseTracingTestIT {
         // create a simple metrics record
         long traceid = 987654;
         MetricsRecord record =
-                createAndFlush(sink, traceid, Span.ROOT_SPAN_ID, 10, "root", 12, 13,
+                createAndFlush(sink, traceid, TracingUtils.ROOT_SPAN_ID, 10, "root", 12, 13,
                     "host-name.value", "test annotation for a span");
 
         // start a reader
@@ -95,7 +96,7 @@ public class PhoenixTraceReaderIT extends BaseTracingTestIT {
         long traceid = 12345;
         List<MetricsRecord> records = new ArrayList<MetricsRecord>();
         MetricsRecord record =
-                createAndFlush(sink, traceid, Span.ROOT_SPAN_ID, 7777, "root", 10, 30,
+                createAndFlush(sink, traceid, TracingUtils.ROOT_SPAN_ID, 7777, "root", 10, 30,
                     "hostname.value", "root-span tag");
         records.add(record);
 
@@ -150,7 +151,7 @@ public class PhoenixTraceReaderIT extends BaseTracingTestIT {
             Iterator<AbstractMetric> metricIter = record.metrics().iterator();
             assertEquals("Got an unexpected span id", metricIter.next().value(), spanInfo.id);
             long parentId = (Long) metricIter.next().value();
-            if (parentId == Span.ROOT_SPAN_ID) {
+            if (parentId == TracingUtils.ROOT_SPAN_ID) {
                 assertNull("Got a parent, but it was a root span!", spanInfo.parent);
             } else {
                 assertEquals("Got an unexpected parent span id", parentId, spanInfo.parent.id);
